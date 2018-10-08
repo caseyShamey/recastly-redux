a79d48b5d2ba8b1e5cffde0be8614c0ce2c37ea6
Casey Shamey <caseyshamey@gmail.com>
2018-10-07 23:54:30 -0400

    Progress on setting up containers

--- src/containers/VideoPlayerContainer.js
+++ src/containers/VideoPlayerContainer.js
@@ -1,8 +1,19 @@
 import { connect } from 'react-redux';
 import VideoPlayer from '../components/VideoPlayer.js';
 
-var VideoPlayerContainer = () => {};
+var mapStateToProps = state => {
+  return {
+    video: state.currentVideo
+  };
+};
 
+// var mapDispatchToProps = dispatch => {
+//   return {
+
+//   };
+// };
+
+var VideoPlayerContainer = connect(mapStateToProps)(VideoPlayer);
 //TODO: define a VideoPlayerContainer component which will hook up your action
 //dispatchers with your VideoPlayer component props.
 
