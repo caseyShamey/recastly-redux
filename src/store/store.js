e85ca8e649aae82816039d2cc27653a69e64f801
Casey Shamey <caseyshamey@gmail.com>
2018-10-07 21:30:52 -0400

    Complete planning state

--- src/store/store.js
+++ src/store/store.js
@@ -3,3 +3,12 @@ import thunk from 'redux-thunk';
 import rootReducer from './../reducers/main.js';
 
 // TODO:  Create your redux store, apply thunk as a middleware, and export it!
+
+//template to consult when writing state
+// export default function configureStore(initialState) {
+//   return createStore(
+//     rootReducer,
+//     initialState,
+//     applyMiddleware(thunk)
+//   );
+// }
