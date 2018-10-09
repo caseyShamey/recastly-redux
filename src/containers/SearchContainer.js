cfaca53543c43885f693623a66663b672554273c
Casey Shamey <caseyshamey@gmail.com>
2018-10-08 21:46:25 -0400

    Complete creating containers

--- src/containers/SearchContainer.js
+++ src/containers/SearchContainer.js
@@ -2,11 +2,29 @@ import React from 'react';
 import Search from './../components/Search.js';
 import { connect } from 'react-redux';
 import handleSearchChange from '../actions/search.js';
-var SearchContainer = () => {};
+
+// var mapDispatchToProps = (dispatch) => {
+//   return {
+//     handleSearchInputChange: () => {
+//       return dispatch(handleSearchChange);
+//     }
+//   };
+// };
+
+var mapDispatchToProps = {
+  handleSearchInputChange: handleSearchChange
+};
+
+var SearchContainer = connect(
+  null,
+  mapDispatchToProps
+)(Search);
 
 //TODO: define a SearchContainer component which will hook up your action
 // dispatchers with your search component props.
 //HINT: use react-redux 'connect' method to generate a container component from
 //state and dispatch mappings.
+
+
 
 export default SearchContainer;
